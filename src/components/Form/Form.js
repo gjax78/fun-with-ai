c7ce03911108f08d093d84f98a6232b693accd4a
Geena Jackson <gjacks303@gmail.com>
2022-05-09 18:47:17 -0600

    Set up structure of Form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Enter prompt here'
+        className='prompt-input'
+        value={}
+        onChange={}
+      />
+
+      <button
+        className='submit-button'
+        onClick={}>
+          SUBMIT
+      </button>
+    </form>
+  )
+}
+
+export default Form
