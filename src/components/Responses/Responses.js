ae191591b0121898818e892099940516d6efe1ff
Geena Jackson <gjacks303@gmail.com>
2022-05-09 18:39:07 -0600

    Create structure of Responses component

---
+++ src/components/Responses/Responses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Responses = () => {
+  return (
+    <div className='response-container'>
+      <h1>Responses: Responses go here</h1>
+    </div>
+  )
+}
+
+export default Responses 
