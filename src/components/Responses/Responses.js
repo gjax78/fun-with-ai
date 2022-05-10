81b97c89cf89759504a3c224a326684af5c62f76
Geena Jackson <gjacks303@gmail.com>
2022-05-09 19:20:11 -0600

    Set up mapped out ResponseCard

--- src/components/Responses/Responses.js
+++ src/components/Responses/Responses.js
@@ -4,14 +4,20 @@ import ResponseCard from '../ResponseCard/ResponseCard'
 const Responses = ({ prompts }) => {
   const allPrompts = prompts.map(prompt => {
     return (
-      <div className='response-container'>
+      <div className='response-container' key={prompt.id}>
         <ResponseCard
-  
+          prompt={prompt.prompt}
+          response={prompt.choices[0].text}
         />
       </div>
     )
   })
 
+  return (
+    <div className='response-container'>
+      {allPrompts}
+    </div>
+  )
 }
 
 export default Responses 
