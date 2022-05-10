6b80ffa50c1db606c8ab97bf13bc22af0c05fe7c
Geena Jackson <gjacks303@gmail.com>
2022-05-09 18:52:08 -0600

    Create POST function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import Form from '../Form/Form'
 import Responses from '../Responses/Responses'
 import fetchAPI from '../../util/apiCalls' 
 
 const App = () => {
+  const [prompts, setPrompts] = useState([])
+
+  const addPrompt = (data) => {
+    fetchAPI.postPrompt(data)
+    .then(data => setPrompts(data))
+  }
+
   return (
     <main className="App">
       <h1>Fun with AI</h1>
