e7ceda204bdac83d77f98c19f995e5bc4b503edb
Geena Jackson <gjacks303@gmail.com>
2022-05-09 19:06:21 -0600

    Add state to handle event change on form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = ({ addPrompt }) => {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (event) => {
+    setText(event.target.value)
+  }
+
   return (
     <form>
       <input
         type='text'
         placeholder='Enter prompt here'
         className='prompt-input'
-        value={}
-        onChange={}
+        value={text}
+        onChange={event => handleTextChange(event)}
       />
 
       <button
