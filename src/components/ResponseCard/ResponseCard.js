32296fdd4de839c9bf73611345dc1eb9407e670e
Geena Jackson <gjacks303@gmail.com>
2022-05-09 18:48:05 -0600

    Set up structure of ResponseCard component

---
+++ src/components/ResponseCard/ResponseCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ResponseCard = () => {
+  return (
+    <div className='card'>
+      <p className='prompt'>Prompt: Prompt goes here</p>
+      <p className='response'>Response: Response goes here</p>
+    </div>
+  )
+}
+
+export default ResponseCard
