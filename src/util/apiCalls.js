2839c8b39ef041cd2a548a12b82212ac2d98c364
Geena Jackson <gjacks303@gmail.com>
2022-05-12 18:18:50 -0600

    Move request object from Form component

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -1,5 +1,14 @@
 const fetchAPI = {
   postPrompt(data) {
+    const data = {
+      prompt: text,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    }
+
     return fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
       method: "POST",
       headers: {
