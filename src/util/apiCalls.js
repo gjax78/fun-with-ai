7ee2c312a42743478d85cbc3c13789c09f3f1cb8
Geena Jackson <gjacks303@gmail.com>
2022-05-13 19:57:23 -0600

    Add error handling to apiCall

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchAPI = {
-  async postPrompt(prompt) {
+  postPrompt(prompt) {
     const data = {
       prompt: prompt,
       temperature: 0.5,
@@ -9,15 +9,20 @@ const fetchAPI = {
       presence_penalty: 0.0,
     }
 
-    const response = await fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+    return fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
       },
       body: JSON.stringify(data),
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error()
+      } else {
+        return response.json()
+      }
     })
-    return await response.json()
   }
 }
 
