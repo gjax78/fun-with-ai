7428e76bafd2e133036defb98db8825133caf9e7
Geena Jackson <gjacks303@gmail.com>
2022-05-15 14:16:02 -0600

    Refactor randomize function by putting list of questions in util file

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,30 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import './Form.css'
 import fetchAPI from '../../util/apiCalls' 
+import data from '../../util/questions' 
 
 const Form = ({ setResponses, setIsLoading }) => {
   const [prompt, setPrompt] = useState('')
   const [engine, setEngine] = useState('')
   const [error, setError] = useState('')
   const [placeholder, setPlaceholder] = useState('')
-  
+
   const handlePromptChange = (event) => {
     setPrompt(event.target.value)
   }
-
+  
   const randomizePlaceholder = () => {
-    const placeholder = [
-      'Try typing in: Translate this into Japanese: Hello, how are you?',
-      'Try typing in: What is the circumference of planet Earth?', 
-      'Try typing in: What is your favorite color?', 
-      'Try typing in: Where is Little Rock located?', 
-      'Try typing in: Who wrote the book Green Eggs and Ham?', 
-      'Try typing in: How much of the human body is made of water?', 
-      'Try typing in: Tell me a random fact.',
-      'Try typing in: Who won the grammy for Best New Artist in 2022?'
-    ]
-    setPlaceholder(placeholder[Math.floor(Math.random() * placeholder.length)])
+    setPlaceholder(data.questions[Math.floor(Math.random() * data.questions.length)])
   }
   
   const handleDropdownChange = (event) => {
