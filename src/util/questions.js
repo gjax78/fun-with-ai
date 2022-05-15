94ae226160ca9739c949bbe3ec70a1d3fb1bf910
Geena Jackson <gjacks303@gmail.com>
2022-05-15 14:15:27 -0600

    Create list of questions

---
+++ src/util/questions.js
@@ -0,0 +1,26 @@
+const data = {
+  questions: [
+  'Try typing in: Translate this into Japanese: Hello, how are you?',
+  'Try typing in: What is the circumference of planet Earth?', 
+  'Try typing in: What\'s the smallest country in the world?', 
+  'Try typing in: What country touches the Indian Ocean, the Arabian Sea, and the Bay of Bengal?', 
+  'Try typing in: What country has the most natural lakes?', 
+  'Try typing in: How much of the human body is made of water?', 
+  'Try typing in: The unicorn is the national animal of which country?',
+  'Try typing in: Who won the grammy for Best New Artist in 2022?', 
+  'Try typing in: In the state of Georgia, it\'s illegal to eat what with a fork?', 
+  'Try typing in: What were the four main characters\' names in the TV series "Golden Girls" that ran from 1985-1992?',
+  'Try typing in: In which city was Jim Morrison buried?',
+  'Try typing in: What colors are the Norwegian flag?',
+  'Try typing in: Name the three primary colors.',
+  'Try typing in: What was Britney Spears\' first song?',
+  'Try typing in: How many phases of the moon are there?',
+  'Try typing in: What is the national dish of Spain?',
+  'Try typing in: Which is the highest waterfall in the world?',
+  'Try typing in: Kodiak island is in which US state?',
+  'Try typing in: Zurich is the largest city in what country?',
+  'Try typing in: Name the world\'s largest ocean.'
+  ]
+} 
+
+export default data
